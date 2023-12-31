6f0561f9ae8c57bb0ede1198ad09dfcdfe46c6c5
chandan <chandan7979ck@gmail.com>
2023-12-31 14:17:39 +0530

    project under dev

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Projects.css';
+
+const Projects = () => {
+  return (
+    <>
+        <section className='projects' id='projects'>
+            <h2 className="boxheading">
+                Projects
+            </h2>
+        </section>
+    </>
+  )
+}
+
+export default Projects
